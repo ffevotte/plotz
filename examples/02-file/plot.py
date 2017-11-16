9e6898a26dbd6cf198bd5712bbd36eea77d59f20
François Févotte <fevotte@gmail.com>
2017-11-16 13:34:55 +0100

    Update example 2 to show real data

--- examples/02-file/plot.py
+++ examples/02-file/plot.py
@@ -12,24 +12,36 @@ from math import sin, pi
 #         csv.write("   ; ".join(["%.7f" % v for v in [x, sin(3*pi*x), sin(4*pi*x)]]) + "\n")
 
 with Plot("plot") as p:
+    p.x.min = 0
+    p.x.max = 20.3
+    p.x.tick = 2
+    p.x.label = r"$\omega$"
+
+    p.y.min = 0
+    p.y.tick = 0.2
+    p.y.tick_format = lambda y: "%.1f" % y
+    p.y.label = r"$\displaystyle\frac{\Vert\phi_1\Vert_2}{\Vert\phi_0\Vert_2}$"
+
     p.title = r"Plotting data from files"
-    p.x.label = "$x$"
-    p.y.label = "$y$"
-    p.y.label_rotate = True
+
+    p.line_style(1, "ultra thick")
+    p.line_style(2, "ultra thick")
+    for line in range(3,8):
+        p.line_style(line, "thick")
 
     #line1
     p.plot(DataFile("mydata.dat"),
-           title=r"$\sin(\pi\,x)$")
+           title=r"Source Iterations")
     #line1
 
     #line2
     p.plot(DataFile("mydata.dat"), col=(0,2),
-           title=r"$\sin(2\,\pi\,x)$")
+           title=r"DSA")
     #line2
 
     #line3
-    p.plot(DataFile("mydata.csv", sep=";", comment="#"),
-           title=r"$\sin(3\,\pi\,x)$")
+    p.plot(DataFile("mydata.csv", sep=";", comment="#"), col=(0,3),
+           title=r"PDSA(6)")
     #line3
 
-    p.legend("south west")
+    p.legend("north east")
